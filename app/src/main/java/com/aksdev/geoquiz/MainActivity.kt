097d811a81d125f04b9d13485e9d92b6e680a338
aktester07 <47987593+aktester07@users.noreply.github.com>
2019-10-14 23:10:30 +0530

    fixed minor typos

--- app/src/main/java/com/aksdev/geoquiz/MainActivity.kt
+++ app/src/main/java/com/aksdev/geoquiz/MainActivity.kt
@@ -2,22 +2,39 @@ package com.aksdev.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
+import android.widget.Toast
 
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity(){
-private lateint var trueButton: Button
-private lateint var falseButton: Button
+private lateinit var trueButton: Button
+private lateinit var falseButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        true_button = findViewById(R.id.true_button)
-        false_button = findViewById(R.id.false_button)
+        trueButton = findViewById(R.id.true_button)
+        falseButton = findViewById(R.id.false_button)
 
-        trueButton.setOnClickListener{view: view->
+        trueButton.setOnClickListener{view: View ->
+            Toast.makeText(
+                this,
+                R.string.correct_toast,
+                Toast.LENGTH_SHORT)
+                .show()
+
         }
+        falseButton.setOnClickListener{view: View ->
+            Toast.makeText(
+                this,
+                R.string.incorrect_toast,
+                Toast.LENGTH_SHORT)
+                .show()
+
+        }
+
 
     }
 }
