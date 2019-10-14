f6c20f30ae434a37941098a497b17d174c573ca4
aktester07 <47987593+aktester07@users.noreply.github.com>
2019-10-14 22:53:50 +0530

    adding button in the MainActivity and toast message in strings.xml

--- app/src/main/java/com/aksdev/geoquiz/MainActivity.kt
+++ app/src/main/java/com/aksdev/geoquiz/MainActivity.kt
@@ -2,11 +2,22 @@ package com.aksdev.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
-class MainActivity : AppCompatActivity() {
+import kotlinx.android.synthetic.main.activity_main.*
 
+
+class MainActivity : AppCompatActivity(){
+private lateint var trueButton: Button
+private lateint var falseButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        true_button = findViewById(R.id.true_button)
+        false_button = findViewById(R.id.false_button)
+
+        trueButton.setOnClickListener{view: view->
+        }
+
     }
 }
